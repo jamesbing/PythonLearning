5720dcef30e06fbce7663f98f66b0a12991d93bf
james <jiabing@mail.nankai.edu.cn>
2015-04-22 12:28:07 +0800

    varible_work_filed
    this code segment demonstrates the work fields of variables~~

---
+++ varible_working_field.py
@@ -0,0 +1,36 @@
+"""
+
+the default values are evaluated at the point of function definition
+in the defining scope
+the default value is evaluated only once
+
+"""
+i = 5
+
+def f(arg=i):
+ print arg
+
+i = 6
+f()
+
+#this code segment above will print 5
+
+"""
+
+heres another example, this following code segement will produce a resutlt as following:
+[1]
+[1,2]
+[1,2,3]
+
+
+"""
+
+def f(a,L=[]):
+ L.append(a)
+ return L
+
+print f(1)
+print f(2)
+print f(3)
+
+#what if you add a print thing inside that function?
